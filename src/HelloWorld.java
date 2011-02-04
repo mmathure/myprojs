e356eff206a8b094f104c44184982483ddcbfe55
Mandar Mathure <mmathure@gmail.com>
2011-02-05 00:10:43 +0530

    Second Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author mandar
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
